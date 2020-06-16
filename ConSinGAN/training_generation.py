1156a7ad9330c548c73cbaf25d31db7c30653da0
LuanVan_2019 <lyphilong7969@gmail.com>
2020-06-16 12:39:45 +0700

    fix error resize image

--- ConSinGAN/training_generation.py
+++ ConSinGAN/training_generation.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 import ConSinGAN.functions as functions
 import ConSinGAN.models as models
+import ConSinGAN.imresize as imresize
 
 
 def train(opt):
@@ -20,10 +21,17 @@ def train(opt):
     print("\t non-linearity: {}".format(opt.activation))
 
     real_a = functions.read_image_a(opt)
+    real_b = functions.read_image_b(opt)
+
+    real_b =functions.torch2uint8(real_b)
+    real_b =imresize.imresize_in(real_b, output_shape=(real_a.shape[2],real_a.shape[3]))
+    #real_b_1 = functions.np2torch(real_b,opt)
+    real_b = functions.np2torch(real_b,opt)
+    #print(real_b_1.shape)
+    
     real_a = functions.adjust_scales2image(real_a, opt) #Caculate size image of first scale
     reals_a = functions.create_reals_pyramid(real_a, opt)
 
-    real_b = functions.read_image_b(opt)
     real_b = functions.adjust_scales2image(real_b, opt)
     reals_b = functions.create_reals_pyramid(real_b, opt)
     
@@ -38,11 +46,15 @@ def train(opt):
     generator_a = init_G(opt)
     fixed_noise_a = []
     noise_amp_a = []
+    fakes_a = []
+    mixs_g_a =[]
 
     #Add generator B
     generator_b = init_G(opt)
     fixed_noise_b = []
     noise_amp_b = []
+    fakes_b =[]
+    mixs_g_b =[]
 
     for scale_num in range(opt.stop_scale+1):
         opt.out_ = functions.generate_dir2save(opt)
@@ -54,6 +66,7 @@ def train(opt):
                 pass
         functions.save_image('{}/real_a_scale.jpg'.format(opt.outf), reals_a[scale_num])
         functions.save_image('{}/real_b_scale.jpg'.format(opt.outf), reals_b[scale_num])
+        #functions.save_image('{}/real_b_test.jpg'.format(opt.outf), real_b_1)
 
 
         d_curr_a = init_D(opt)
@@ -64,10 +77,16 @@ def train(opt):
             generator_a.init_next_stage()
             generator_b.init_next_stage()
 
-        writer = SummaryWriter(log_dir=opt.outf)
-        fixed_noise_a, fixed_noise_b, noise_amp_a,noise_amp_b, generator_a, generator_b, d_curr_a, d_curr_b = train_single_scale(d_curr_a, d_curr_b, 
-                                                              generator_a, generator_b, reals_a, reals_b, fixed_noise_a, fixed_noise_b, noise_amp_a,noise_amp_b, opt, scale_num, writer)
-
+        #writer = SummaryWriter(log_dir=opt.outf)
+        fixed_noise_a, fixed_noise_b, noise_amp_a,noise_amp_b, generator_a, generator_b, d_curr_a, d_curr_b, fakes_a, fakes_b, mixs_g_a, mixs_g_b = train_single_scale(d_curr_a, d_curr_b, 
+                                                              generator_a, generator_b, reals_a, reals_b, 
+                                                              fixed_noise_a, fixed_noise_b, 
+                                                              noise_amp_a,noise_amp_b, 
+                                                              fakes_a, fakes_b, 
+                                                              mixs_g_a,mixs_g_b,
+                                                              opt, scale_num)
+        print("Chiều dài fakes a là: ", len(fakes_a))
+        print("Chiều dài fakes b là: ",len(fakes_b))
         torch.save(fixed_noise_a, '%s/fixed_noise_a.pth' % (opt.out_))
         torch.save(fixed_noise_b, '%s/fixed_noise_b.pth' % (opt.out_))
         torch.save(generator_a, '%s/G_a.pth' % (opt.out_))
@@ -76,14 +95,15 @@ def train(opt):
         torch.save(noise_amp_a, '%s/noise_amp_a.pth' % (opt.out_))
         torch.save(noise_amp_b, '%s/noise_amp_b.pth' % (opt.out_))
         del d_curr_a, d_curr_b
-    writer.close()
+    #writer.close()
     return
 
 
-def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_noise_a,fixed_noise_b, noise_amp_a,noise_amp_b, opt, depth, writer):
+def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_noise_a,fixed_noise_b, noise_amp_a,noise_amp_b, fakes_a,fakes_b, mixs_g_a, mixs_g_b, opt, depth):
     reals_shapes = [real.shape for real in reals_a]
     real_a = reals_a[depth]
     real_b = reals_b[depth]
+
 
     # Hai sieu tham so cua loss rec vÃ  cycle
     alpha = opt.alpha
@@ -115,9 +135,8 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
     # define optimizers, learning rate schedulers, and learning rates for lower stages
     ###########################
     # setup optimizers for D
-    optimizerD_a = optim.Adam(netD_a.parameters(), lr=opt.lr_d, betas=(opt.beta1, 0.999))
-    optimizerD_b = optim.Adam(netD_b.parameters(), lr=opt.lr_d, betas=(opt.beta1, 0.999))
-
+    optimizerD = optim.Adam(list(netD_a.parameters()) + list(netD_b.parameters()), lr=opt.lr_d, betas=(opt.beta1, 0.999))
+    
     # setup optimizers for G
     # remove gradients from stages that are not trained
     for block in netG_a.body[:-opt.train_depth]:
@@ -142,14 +161,14 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
         parameter_list_b += [{"params": netG_b.head.parameters(), "lr": opt.lr_g * (opt.lr_scale**depth)}]
     parameter_list_a += [{"params": netG_a.tail.parameters(), "lr": opt.lr_g}]
     parameter_list_b += [{"params": netG_b.tail.parameters(), "lr": opt.lr_g}]
-    optimizerG_a = optim.Adam(parameter_list_a, lr=opt.lr_g, betas=(opt.beta1, 0.999))
-    optimizerG_b = optim.Adam(parameter_list_b, lr=opt.lr_g, betas=(opt.beta1, 0.999))
+    optimizerG = optim.Adam(parameter_list_a + parameter_list_b, lr=opt.lr_g, betas=(opt.beta1, 0.999))
+    #optimizerG_b = optim.Adam(parameter_list_b, lr=opt.lr_g, betas=(opt.beta1, 0.999))
 
     # define learning rate schedules
-    schedulerD_a = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerD_a, milestones=[0.8*opt.niter], gamma=opt.gamma)
-    schedulerD_b = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerD_b, milestones=[0.8*opt.niter], gamma=opt.gamma)
-    schedulerG_a = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerG_a, milestones=[0.8*opt.niter], gamma=opt.gamma)
-    schedulerG_b = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerG_b, milestones=[0.8*opt.niter], gamma=opt.gamma)
+    schedulerD = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerD, milestones=[0.8*opt.niter], gamma=opt.gamma)
+    #schedulerD_b = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerD_b, milestones=[0.8*opt.niter], gamma=opt.gamma)
+    schedulerG = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerG, milestones=[0.8*opt.niter], gamma=opt.gamma)
+    #schedulerG_b = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerG_b, milestones=[0.8*opt.niter], gamma=opt.gamma)
 
     ############################
     # calculate noise_amp
@@ -181,7 +200,7 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
     # start training
     _iter = tqdm(range(opt.niter))
     for iter in _iter:
-        _iter.set_description('stage [{}/{}]:'.format(depth, opt.stop_scale))
+        #_iter.set_description('stage [{}/{}]:'.format(depth, opt.stop_scale))
 
         ############################
         # (0) sample noise for unconditional generation
@@ -206,11 +225,21 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
             if j == opt.Dsteps - 1:
                 fake_a = netG_a(noise, reals_shapes, noise_amp_a)
                 
+                
             else:
                 with torch.no_grad():
                     fake_a = netG_a(noise, reals_shapes, noise_amp_a)
-                    
-            mix_g_a = netG_a(fake_a, reals_shapes,fake_a)
+            
+            #Viết hàm để tránh trường hợp add vào quá nhiều tạo ra tràn bộ nhớ
+            if(len(fakes_a) == depth):
+                fakes_a.append(fake_a)
+            else:
+                fakes_a.pop(depth)
+                fakes_a.append(fake_a)
+            
+            print("Fakes_a: {}".format([r.shape for r in fakes_a]))
+            mix_g_a = netG_a(fakes_a, reals_shapes,noise_amp_a)
+            mixs_g_a.append(mix_g_a)
             output_a = netD_a(mix_g_a.detach())
             output_a2 = netD_a(fake_a.detach())
             errD_fake_a = output_a.mean() + output_a2.mean()
@@ -218,7 +247,7 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
             gradient_penalty_a = functions.calc_gradient_penalty(netD_a, real_a, mix_g_a, opt.lambda_grad, opt.device)
             gradient_penalty_a += functions.calc_gradient_penalty(netD_a, real_a, fake_a, opt.lambda_grad, opt.device)
             errD_total_a = errD_real + errD_fake_a + gradient_penalty_a
-            errD_total_a.backward()
+            errD_total_a.backward(retain_graph=True)
 
             #############################
             ####      Train D_b      ####
@@ -231,20 +260,29 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
 
             # train with fake
             if j == opt.Dsteps - 1:
-                fake_b = netG(noise, reals_shapes, noise_amp_b)
+                fake_b = netG_b(noise, reals_shapes, noise_amp_b)
             else:
                 with torch.no_grad():
-                    fake_b = netG(noise, reals_shapes, noise_amp_b)
+                    fake_b = netG_b(noise, reals_shapes, noise_amp_b)
 
+            if(len(fakes_b) == depth):
+                fakes_b.append(fake_b)
+            else:
+                fakes_b.pop(depth)
+                fakes_b.append(fake_b)
+
+            print("Fakes_b: {}".format([r.shape for r in fakes_b]))
+            mix_g_b = netG_b(fakes_a, reals_shapes, noise_amp_a)
+            mixs_g_b.append(mix_g_b)
             output_b = netD_b(mix_g_b.detach()) 
             output_b2 = netD_b(fake_b.detach())
             errD_fake_b = output_b.mean() + output_b2.mean()
 
             gradient_penalty_b = functions.calc_gradient_penalty(netD_b, real_b, fake_b, opt.lambda_grad, opt.device)
-            gradient_penalty_b += calc_gradient_penalty(netD_b, real_b, mix_g_b, opt.lambda_grad, opt.device)
+            gradient_penalty_b += functions.calc_gradient_penalty(netD_b, real_b, mix_g_b, opt.lambda_grad, opt.device)
             errD_total_b = errD_real + errD_fake_b + gradient_penalty_b
-            errD_total_b.backward()
-            
+            errD_total_b.backward(retain_graph=True)
+
             optimizerD.step()
 
         ############################
@@ -261,13 +299,15 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
         output_b2 = netD_a(fake_b)
         errG_b = -output_b.mean() - output_b2.mean()
 
+        print(len(noise_amp_a))
+
         if alpha != 0:
             rec_loss = nn.MSELoss()
-            rec_a = netG(fixed_noise_a, reals_shapes_a, noise_amp_a) #xem lai
-            rec_loss_a = alpha * loss(rec_a, real_a)
+            rec_a = netG_a(fixed_noise_a, reals_shapes, noise_amp_a) #xem lai
+            rec_loss_a = alpha * rec_loss(rec_a, real_a)
 
-            rec_b = netG_b(fixed_noise_b, reals_shapes_b, noise_amp_b)
-            rec_loss_b = alpha * loss(rec_b, real_b)
+            rec_b = netG_b(fixed_noise_b, reals_shapes, noise_amp_b)
+            rec_loss_b = alpha * rec_loss(rec_b, real_b)
         else:
             rec_loss_a = 0
             rec_loss_b = 0
@@ -275,20 +315,20 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
         if beta != 0:
             cycle_loss = nn.MSELoss()
             
-            cycle_a = G_a(mix_g_b)
+            cycle_a = netG_a(mixs_g_b,reals_shapes, noise_amp_a)
             cycle_loss_a = beta * cycle_loss(cycle_a, fake_a)
             
-            cycle_b = G_b(mix_g_a)
+            cycle_b = netG_b(mixs_g_a,reals_shapes, noise_amp_b)
             cycle_loss_b = beta * cycle_loss(cycle_b, fake_b)
         else:
             cycle_loss_a = 0
             cycle_loss_b = 0
                 
         errG_total_a = errG_a + rec_loss_a + cycle_loss_a
-        errG_total_a.backward()
+        errG_total_a.backward(retain_graph=True)
 
         errG_total_b = errG_b + rec_loss_b + cycle_loss_b
-        errG_total_b.backward()
+        errG_total_b.backward(retain_graph=True)
 
         for _ in range(opt.Gsteps):
             optimizerG.step()
@@ -297,22 +337,23 @@ def train_single_scale(netD_a, netD_b, netG_a, netG_b, reals_a, reals_b, fixed_n
         ####  (3) Log Results   ####
         ############################
         if iter % 250 == 0 or iter+1 == opt.niter:
-            writer.add_scalar('Loss/train/D/real/{}'.format(j), -errD_real.item(), iter+1)
-            writer.add_scalar('Loss/train/D/fake/{}'.format(j), errD_fake.item(), iter+1)
-            writer.add_scalar('Loss/train/D/gradient_penalty/{}'.format(j), gradient_penalty.item(), iter+1)
-            writer.add_scalar('Loss/train/G/gen', errG.item(), iter+1)
-            writer.add_scalar('Loss/train/G/reconstruction', rec_loss.item(), iter+1)
+            print(f"[{iter}]/20000 tại scale [depth]")
+            #writer.add_scalar('Loss/train/D_a/real/{}'.format(j), -errD_real.item(), iter+1)
+            #writer.add_scalar('Loss/train/D_a/fake/{}'.format(j), errD_fake.item(), iter+1)
+            #writer.add_scalar('Loss/train/D/gradient_penalty/{}'.format(j), gradient_penalty.item(), iter+1)
+            #writer.add_scalar('Loss/train/G/gen', errG.item(), iter+1)
+            #writer.add_scalar('Loss/train/G/reconstruction', rec_loss.item(), iter+1)
         if iter % 500 == 0 or iter+1 == opt.niter:
-            functions.save_image('{}/fake_sample_{}.jpg'.format(opt.outf, iter+1), fake.detach())
-            functions.save_image('{}/reconstruction_{}.jpg'.format(opt.outf, iter+1), rec.detach())
-            generate_samples(netG, opt, depth, noise_amp, writer, reals, iter+1)
+            functions.save_image('{}/fake_sample_a{}.jpg'.format(opt.outf, iter+1), fake_a.detach())
+            functions.save_image('{}/reconstruction_a{}.jpg'.format(opt.outf, iter+1), rec_a.detach())
+           # generate_samples(netG_a, opt, depth, noise_amp, writer, reals, iter+1)
 
         schedulerD.step()
         schedulerG.step()
         # break
 
     functions.save_networks(netG, netD, z_opt, opt)
-    return fixed_noise_a, fixed_noise_b, noise_amp_a, noise_amp_b, netG_a, netG_b, netD_a, netD_b
+    return fixed_noise_a, fixed_noise_b, noise_amp_a, noise_amp_b, netG_a, netG_b, netD_a, netD_b, fakes_a,fakes_b, mixs_g_a, mixs_g_b
 
 
 def generate_samples(netG, opt, depth, noise_amp, writer, reals, iter, n=25):
